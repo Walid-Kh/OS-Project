41f378964144417c04a7221a180330eeaf7ffc25
mostafa <mostafa.elsayed.2002@gmail.com>
2023-04-10 00:35:29 +0000

    process_generator.c almost done (there is only a small error in Queue.h)

--- Phase-I/Queue.h
+++ Phase-I/Queue.h
@@ -3,19 +3,39 @@
 #include <stdlib.h>
 
 
+
+
+struct process
+{
+    int id;
+    int arrival;
+    int runtime;
+    int priority;
+};
+
+
 struct Node
 {
-  int data;
-  struct Node* Next;
+    struct process process;
+    struct Node * Next;
 
 };
 
 struct Queue
 {
    struct Node* Front;
-    struct Node* Rear;
-    int count;
+   struct Node* Rear;
+   int count;
 };
+
+void  queueConstractor(struct Queue q)
+{
+    q.Front=NULL;
+    q.Rear=NULL;
+    q.count=0;
+}
+
+
 
 bool isEmpty(struct Queue q) {
     if (q.Front == NULL)
@@ -23,35 +43,41 @@ bool isEmpty(struct Queue q) {
     return 0;
 }
 
-void enqueue(struct Queue q,int value){
+void enqueue(struct Queue q,struct process p){
 
     struct Node *n ;
     n =(struct Node*) malloc(sizeof(struct Node));
+
+    n->process=p;
 
     if(q.count==0)
     {
         q.Front=n;
         q.Rear=n;
+        n->process=p;
     }
-    else{
 
-        q.Rear->Next=n;
+    else{
+        q.Rear->Next=n; //there is a error here
         q.Rear=n;
-        n->data=value;
         q.count=q.count+1;
 
-    }
+     }
 
 }
 
 
-void dequeue(struct Queue q){
+struct process dequeue (struct Queue q){
 
    struct Node *temp;
+   struct process tempProcess;
+    tempProcess=q.Front->process;
+
     temp=q.Front;
     q.Front=q.Front->Next;
     free(temp);
     q.count=q.count-1;
+    return tempProcess;
 
 }
 
--- Phase-I/process_generator.c
+++ Phase-I/process_generator.c
@@ -1,27 +1,115 @@
 #include "headers.h"
+#include "Queue.h"
+
+#define  PG_SH_KEY 90
 
 void clearResources(int);
+
+struct Algomsg    // message structure of Algorithm selected
+{
+    int Algonum;
+};
+struct processmsg  // message structure of process
+{
+    struct process process;
+};
+
 
 int main(int argc, char *argv[])
 {
     signal(SIGINT, clearResources);
     // TODO Initialization
-    // 1. Read the input files.
+    // 1. Read the input files. (done)
+    char line[30];
+    FILE *fp;
+    fp = fopen("processes.txt", "r");   //Open processes.txt
+    fgets(line,sizeof(line),fp);    //Get first line and ignore it
+    struct Queue q; // the Queue of processes
+    queueConstractor(q); //call the constractor;
+    struct process p; // is already defined in Queue.h
+
+    int z;
+    while (!feof(fp))   //Get the data for each process and creat it
+    {
+        struct process p; // is already defined in Queue.h
+
+        fscanf(fp, "%d", &z);  //#id
+        p.id = z;
+        fscanf(fp, "%d", &z); //arrival
+        p.arrival = z;
+        fscanf(fp, "%d", &z); //runtime
+        p.runtime = z;
+        fscanf(fp, "%d", &z); //priority
+        p.priority = z;
+
+      //  enqueue(q, p); there is a error here
+    }
+    fclose(fp);
+
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-    // 3. Initiate and create the scheduler and clock processes.
+    printf("Which scheduling algorithm do you want?\n");
+    printf("1. Non-preemptive Highest Priority First (HPF).\n");
+    printf("2. Shortest Remaining time Next (SRTN).\n");
+    printf("3. Round Robin (RR).\n");
+    printf("======Enter the number of the algorithm you want=====\n");
+    int num,timeSlice;
+    scanf("%d",&num);
+     if(num==3)
+     {
+         printf("You choice Round Robin (RR) algorithm ,please enter time slice in second unit");
+         scanf("%d",timeSlice);
+     }
+
+    //=======================send the selected algorithm to scheduler.c==================
+    int Qid;
+    Qid = msgget(PG_SH_KEY, 0666 | IPC_CREAT);
+    if (Qid == -1)
+    {
+        perror("Error in create the Queue");
+        exit(-1);
+    }
+    struct Algomsg A ;
+    A.Algonum=num;
+    int send_val = msgsnd(Qid, &A, sizeof(A.Algonum), !IPC_NOWAIT);
+    if (send_val == -1)
+        perror("Error in send");
+    //====================================================================================
+
+
+     // 3. Initiate and create the scheduler and clock processes.
+      int pid =fork();
+      if(pid==0)
+      execl("./build/clk.out","clk",NULL);
     // 4. Use this function after creating the clock process to initialize clock
-    initClk();
-    // To get time use this
-    int x = getClk();
-    printf("current time is %d\n", x);
+      initClk();
+     //To get time use this
+      int x = getClk();
+      printf("current time is %d\n", x);
+
+
     // TODO Generation Main Loop
-    // 5. Create a data structure for processes and provide it with its parameters.
+    // 5. Create a data structure for processes and provide it with its parameters. (done in the above part)
     // 6. Send the information to the scheduler at the appropriate time.
+    while (!isEmpty(q))
+    {
+        while (q.Front->process.arrival==getClk())
+         {
+             struct process temp;
+             temp=dequeue(q);
+             // =====> send it to scheduler <=======//
+             struct processmsg p ;
+              p.process=temp;
+             send_val = msgsnd(Qid, &p, sizeof(p.process), !IPC_NOWAIT);
+             if (send_val == -1)
+                 perror("Error in send");
+         }
+    }
+
     // 7. Clear clock resources
-    destroyClk(true);
+     destroyClk(true);
 }
 
 void clearResources(int signum)
 {
-    // TODO Clears all resources in case of interruption
+    //TODO Clears all resources in case of interruption
 }
